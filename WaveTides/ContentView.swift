9167e9b2f092d333a84ee4a62fc2c7410e4e13c4
pvrahul271199 <pvrahul.271199@gmail.com>
2024-11-30 12:11:41 +0530

    Initial Commit

---
+++ WaveTides/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WaveTides
+//
+//  Created by Rahul P V on 30/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WaveTides/WaveTidesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaveTidesApp.swift
+//  WaveTides
+//
+//  Created by Rahul P V on 30/11/24.
+//
+
+import SwiftUI
+
+@main
+struct WaveTidesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
